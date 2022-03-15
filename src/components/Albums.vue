b089458e8a4abd42c32214b9a1a0f34d97f748ec
fabiochidda <fabio.chidda99@gmail.com>
2022-03-15 02:21:55 +0100

    Impostato il main e creato il componente per gli album

---
+++ src/components/Albums.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+      
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MsAlbums',
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,15 +1,33 @@
 <template>
     <main>
-
+        <div class="container">
+            <div class="album-container">
+                <Albums />
+            </div>
+        </div>
     </main>
 </template>
 
 <script>
+import Albums from './Albums.vue'
+
 export default {
     name: 'MsMain',
+    components: {
+        Albums,
+    }
 }
 </script>
 
 <style scoped lang="scss">
+
+.container {
+    height: calc(100vh - 88px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #1E2D3B;
+}
+
 
 </style>
