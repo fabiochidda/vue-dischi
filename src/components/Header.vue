7ab36777701c34cd1fb4434649730cd985ad8ffb
fabiochidda <fabio.chidda99@gmail.com>
2022-03-15 01:48:49 +0100

    Modificato lo stile generale ed aggiunto l'header con il logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
     <header>
-
+        <div class="container">
+            <figure>
+                <img src="../assets/spotify-64.png" alt="">
+            </figure>
+        </div>
     </header>
 </template>
 
@@ -12,5 +16,15 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+.container {
+    width: 100%;
+    padding: 10px;
+    background-color: #2C3741;
+
+    img {
+        max-width: 100%;
+    }
+}
 
 </style>
