c0609cd5303b1da143dfc2c20a533538fa971946
fabiochidda <fabio.chidda99@gmail.com>
2022-03-15 02:43:09 +0100

    Installato axios e ricavato tutto il necessario dall'API

--- src/components/Albums.vue
+++ src/components/Albums.vue
@@ -1,12 +1,55 @@
 <template>
   <div class="container">
-      
+      <div class="card-wrapper">
+          <div v-for="(el, i) in arrayAlbums" :key="i"  class="card">
+              <div class="image">
+                  <figure>
+                      <img :src="el.poster" alt="">
+                  </figure>
+              </div>
+              <div class="album-name">
+                  <h3>{{el.title}}</h3>
+              </div>
+              <div class="album-artist">
+                  <p>{{el.author}}</p>
+              </div>
+              <div class="album-genre">
+                  <p>{{el.genre}}</p>
+              </div>
+              <div class="album-year">
+                  <p>{{el.year}}</p>
+              </div>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
     name: 'MsAlbums',
+    data() {
+        return {
+            arrayAlbums: []
+        }
+    },
+    methods: {
+        albumsRequest: function() {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then( answ => {
+
+                this.arrayAlbums = answ.data.response
+
+            })
+
+        }
+    },
+    created() {
+        this.albumsRequest()
+    }
 
 }
 </script>
